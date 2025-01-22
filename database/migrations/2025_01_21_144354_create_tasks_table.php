3c75b7db2fbb65cea01c0d3e6c79dca849ae23a9
Okuda-Takuya-0223 <tamiwomax@gmail.com>
2025-01-22 09:17:23 +0900

    db:seed エラー

--- database/migrations/2025_01_21_144354_create_tasks_table.php
+++ database/migrations/2025_01_21_144354_create_tasks_table.php
@@ -12,9 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->increments("id");
             $table->string("title");
             $table->text("body");
+            $table->timestamps();
         });
     }
 
