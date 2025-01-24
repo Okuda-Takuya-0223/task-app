abbc2214f9a2d749215cfee1566664f149ccb1f3
Okuda-Takuya-0223 <tamiwomax@gmail.com>
2025-01-24 11:27:05 +0900

    課題修正

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -22,7 +22,7 @@
         {{ $task->body }}
     </div>
 
-    <div class="button-group">
+    <div class="button-groupB">
         <button onclick="location.href='{{ route('tasks.index') }}'">一覧へ戻る</button>
         <button onclick="location.href='{{ route('tasks.edit', $task) }}'">編集する</button>
         <form action="{{ route('tasks.destroy', $task) }}" method="post">
